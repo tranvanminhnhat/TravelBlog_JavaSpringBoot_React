70e49c49cd17bfeabb9077df1f0ef0afa2a616a6
Minh Nhat <tranvanminhnhat99@gmail.com>
2024-10-16 21:26:22 +0700

    update

--- frontend/frontend/src/app/login/page.tsx
+++ frontend/frontend/src/app/login/page.tsx
@@ -1,7 +1,17 @@
-export default function LoginPage() {
-    return (
-        <h1>
-            Login
-        </h1>
-    )
-}
+import React from "react";
+import LoginComponent from "@/components/pages/Login";
+import { NextPage } from "next";
+import FooterComponent from "@/components/layouts/Footer";
+import HeaderComponent from "@/components/layouts/Header";
+
+const Login: NextPage = () => {
+  return (
+    <>
+      <HeaderComponent></HeaderComponent>
+      <LoginComponent></LoginComponent>
+      <FooterComponent></FooterComponent>
+    </>
+  );
+};
+
+export default Login;
---
+++ frontend/frontend/src/components/layouts/Footer/index.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const FooterComponent = () => {
+    return (
+        <>
+                <div className={styles.container}>
+                        Footer
+                </div>
+        </>
+    )
+}
+
+export default FooterComponent;
